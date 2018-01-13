a80ecdffbcc9683b6f61a1c05a82ca9b87fb9f71
sarasate <luis@superlumen.io>
2018-01-13 01:01:29 +0100

    Increase NavBar height

--- src/elements/NavBar/NavBar.js
+++ src/elements/NavBar/NavBar.js
@@ -2,12 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  height: 2em;
-  line-height: 2em;
-  position: relative;
-  padding: 0.5rem 1rem;
   border-bottom: 1px solid rgba(34, 36, 38, 0.15);
+  line-height: 2.875em;
   min-height: 2.875em;
+  padding: 0.5rem 1rem;
+  position: relative;
 `;
 
 const NavBar = props => <Container>{props.children}</Container>;
