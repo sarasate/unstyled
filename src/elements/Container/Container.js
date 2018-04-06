754fefefdb8ef18b1f34df6fb8fb08278295bf20
sarasate <luis@superlumen.io>
2018-04-06 15:53:45 +0200

    Add responsiveness for Container

--- src/elements/Container/Container.js
+++ src/elements/Container/Container.js
@@ -8,6 +8,34 @@ const Container = styled.div`
   display: block;
   margin: 0 auto;
   max-width: 960px;
+
+  /* Mobile */
+  @media only screen and (max-width: 767px) {
+    width: auto;
+    margin-left: 1em;
+    margin-right: 1em;
+  }
+
+  /* Tablet */
+  @media only screen and (min-width: 768px) and (max-width: 991px) {
+    width: 723px;
+    margin-left: auto !important;
+    margin-right: auto !important;
+  }
+
+  /* Small Monitor */
+  @media only screen and (min-width: 992px) and (max-width: 1199px) {
+    width: 933px;
+    margin-left: auto !important;
+    margin-right: auto !important;
+  }
+
+  /* Large Monitor */
+  @media only screen and (min-width: 1200px) {
+    width: 1127px;
+    margin-left: auto !important;
+    margin-right: auto !important;
+  }
 `;
 
 export default Container;
