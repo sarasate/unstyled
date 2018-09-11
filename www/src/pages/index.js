e346e3530cf86011e78ed843f1d2be0bd58c8531
sarasate <luis@superlumen.io>
2018-09-11 17:31:37 +0200

    chore: symlink `unstyled` to gatsby

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import { Header, Text } from 'unstyled'
+import { Container, Heading, Text } from 'unstyled'
+import styled from 'styled-components'
 
-import Layout from '../components/layout'
-
-const IndexPage = () => (
-  <Layout>
-    <Header>
-      React components with minimal styling, easily to extend. Based on
-      styled-components.
-    </Header>
+const IndexPage = () => [
+  <Head id="head">
+    <Container>
+      <Heading color="white">
+        React components with minimal styling, easily to extend. Based on
+        styled-components.
+      </Heading>
+    </Container>
+  </Head>,
+  <Container id="content">
     <Text>
       Introduction After years of prototyping and building dozens of apps,
       mainly for clients, I got tired of using CSS Frameworks like Bootstrap or
@@ -22,7 +25,21 @@ const IndexPage = () => (
       decided to share my components in a dedicated library because of.. well..
       DRY!!!
     </Text>
-  </Layout>
-)
+  </Container>,
+]
 
 export default IndexPage
+
+const Head = styled.div`
+  background-color: rebeccapurple;
+  height: 400px;
+  margin: 0;
+  padding: 1rem 0;
+  text-align: center;
+  transform: matrix(1, 0, 0, 1, 0, 0);
+  transition-delay: 0s, 0s;
+  transition-duration: 6s, 6s;
+  transition-property: background, opacity;
+  transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.4),
+    cubic-bezier(0.68, -0.55, 0.265, 1.4);
+`
