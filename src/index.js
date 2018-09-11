c8b0a74ed1a32ee86a713f2247340700eb952fe0
sarasate <luis@superlumen.io>
2018-09-11 09:22:49 +0200

    refactor: remove styled-normalize

--- src/index.js
+++ src/index.js
@@ -1,30 +1,28 @@
-import "./normalize.js";
-
 /* Themes */
-export { default as DefaultTheme } from "./themes/DefaultTheme";
+export { default as DefaultTheme } from './themes/DefaultTheme'
 export {
-  default as StyledComponentsTheme
-} from "./themes/StyledComponentsTheme";
+  default as StyledComponentsTheme,
+} from './themes/StyledComponentsTheme'
 
 /* Collections */
-export { default as Grid } from "./collections/Grid";
-export { default as Menu } from "./collections/Menu";
-export { default as Message } from "./collections/Message";
+export { default as Grid } from './collections/Grid'
+export { default as Menu } from './collections/Menu'
+export { default as Message } from './collections/Message'
 
 /* Elements */
-export { default as Badge } from "./elements/Badge";
-export { default as Button } from "./elements/Button";
-export { default as Container } from "./elements/Container";
-export { default as Header } from "./elements/Header";
-export { default as Image } from "./elements/Image";
-export { default as Label } from "./elements/Label";
-export { default as Loader } from "./elements/Loader";
-export { default as NavBar } from "./elements/NavBar";
-export { default as Segment } from "./elements/Segment";
+export { default as Badge } from './elements/Badge'
+export { default as Button } from './elements/Button'
+export { default as Container } from './elements/Container'
+export { default as Header } from './elements/Header'
+export { default as Image } from './elements/Image'
+export { default as Label } from './elements/Label'
+export { default as Loader } from './elements/Loader'
+export { default as NavBar } from './elements/NavBar'
+export { default as Segment } from './elements/Segment'
 
 /* Views */
-export { default as Card } from "./views/Card";
-export { default as Item } from "./views/Item";
+export { default as Card } from './views/Card'
+export { default as Item } from './views/Item'
 
 /* Core */
-export * from "./core/";
+export * from './core/'
