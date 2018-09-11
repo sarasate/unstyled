b0ddf8f6ffe0763aaaf82eadf1b71c4c988d6f89
sarasate <luis@superlumen.io>
2018-09-11 17:38:19 +0200

    refactor: cleanup

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Heading, Text } from 'unstyled'
 import styled from 'styled-components'
+import 'components/layout.css'
 
 const IndexPage = () => [
   <Head id="head">
