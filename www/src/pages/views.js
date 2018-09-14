55f7d80d9c60cfcc65f6131cd23cc89c6da8e766
sarasate <luis@superlumen.io>
2018-09-14 22:09:36 +0200

    docs: add views doc

--- www/src/pages/views.js
+++ www/src/pages/views.js
@@ -1,14 +1,47 @@
 import React from 'react'
 import Layout from 'components/layout.js'
 import { Header } from 'unstyled'
+import { Card, Item } from '../../../src/views'
+import { Heading, Image, Input, Link, Text } from 'unstyled'
 
 const ViewsPage = props => {
   return (
     <Layout>
+      <h1>Views</h1>
       <Header>
         Complex elements which can be used standalone or in lists like Card or
         Items
       </Header>
+      {/* Card */}
+      <Heading as="h2">Card</Heading>
+      <Card>
+        <Image src="https://react.semantic-ui.com/images/wireframe/image.png" />
+        <Card.Content>
+          <Card.Header>Card Title</Card.Header>
+          <Card.Meta>Meta Text</Card.Meta>
+          <Card.Description>This is a sample card.</Card.Description>
+        </Card.Content>
+      </Card>
+      {/* Item */}
+      <Heading as="h2">Item</Heading>
+      <Item>
+        <Item.Content>
+          <Item.Header>Item 1</Item.Header>
+          <Item.Text>Item Text</Item.Text>
+        </Item.Content>
+      </Item>
+      <Item>
+        <Item.Content>
+          <Item.Header>Item 2</Item.Header>
+          <Item.Text>Item Text</Item.Text>
+        </Item.Content>
+      </Item>
+      <Item>
+        <Item.Content>
+          <Item.Header>Item 3</Item.Header>
+          <Item.Text>Item Text </Item.Text>
+        </Item.Content>
+      </Item>
     </Layout>
   )
 }
