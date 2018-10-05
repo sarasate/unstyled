1e420b5eef72a5cd2942537caf0d4aef252a24b5
sarasate <luis@superlumen.io>
2018-10-05 18:34:08 +0200

    feat: change `Label` to a semantic label

--- src/core/Label.js
+++ src/core/Label.js
@@ -5,17 +5,24 @@ import DefaultTheme from '../themes/DefaultTheme'
  * A Basic Label representing a HTML form label.
  * TODO refactor Label as Form.Label and use this as general label component
  */
-const Label = styled.label`
+const Label = styled.span`
+  background-color: ${props =>
+    props.color ? props.color : props.theme.secondaryColor};
   border: none;
-  color: ${({ theme }) => theme.primaryColor};
+  border-radius: 0.125em;
+  // TODO solve color/bgColor diff with chroma.js or similar
+  color: ${props => (props.color ? 'white' : props.theme.primaryColor)};
   display: inline-block;
   font-family: ${props => props.theme.fontFamily};
   font-size: 1em;
   font-style: normal;
   font-weight: bold;
   line-height: 1em;
-  margin: 0 0.125em;
+  margin: 0em 0.125em;
   min-height: 1em;
+  padding: 0.5833em 0.833em;
+  text-align: center;
+  text-decoration: none;
   vertical-align: baseline;
 `
 
--- www/src/pages/core/label.js
+++ www/src/pages/core/label.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Layout from 'components/Layout.js'
-import { Heading, Text } from 'unstyled'
+import { Heading, Label, Text } from 'unstyled'
 import Meta from '../../components/Meta'
 
 const LabelPage = () => (
   <Layout>
     <Meta data={{ name: 'Label', description: 'Label component' }} />
     <Heading>Label</Heading>
-    <Text>TODO Refactor `Label` component</Text>
+    <Text>A `Label` displays highlighted classified content.</Text>
+    <Label>Label</Label>
+    <Label color="rebeccapurple">Label</Label>
+    <Label color="red">Label</Label>
+    <Label color="green">Label</Label>
   </Layout>
 )
 
