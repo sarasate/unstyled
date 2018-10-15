41ec0ccaf96aad354e06f417a3d0c3649e4acc19
sarasate <luis@superlumen.io>
2018-10-15 23:11:18 +0200

    chore: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,23 @@
-import nodeResolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
+import nodeResolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
-  input: "src/index.js",
+  input: 'src/index.js',
   output: {
-    file: "dist/unstyled.cjs.js",
-    format: "cjs"
+    file: 'dist/unstyled.cjs.js',
+    format: 'cjs',
   },
-  external: ["react", "prop-types", "styled-components", "styled-normalize"],
+  external: ['react', 'prop-types', 'styled-components', 'styled-normalize'],
   plugins: [
     nodeResolve(),
-    // commonjs({
-    //   ignoreGlobal: true
-    // }),
+    commonjs({
+      // ignoreGlobal: true,
+      include: 'node_modules/**',
+    }),
     babel({
-      exclude: "node_modules/**",
-      plugins: ["external-helpers"]
-    })
-  ]
-};
+      exclude: 'node_modules/**',
+      plugins: ['external-helpers'],
+    }),
+  ],
+}
