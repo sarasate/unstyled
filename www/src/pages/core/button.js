a844f137d3787b3147afc88933f5fdac363c2735
sarasate <luis@superlumen.io>
2018-10-21 10:30:16 +0200

    docs: more button examples

--- www/src/pages/core/button.js
+++ www/src/pages/core/button.js
@@ -8,14 +8,30 @@ const ButtonPage = () => (
     <Meta data={{ name: 'Button', description: 'Button component' }} />
     <Heading>Button</Heading>
     <Text>Buttons for user interactions. Implements a HTML `button` tag. </Text>
-    <Heading level={2}> A standard button</Heading>
-    <Button>Button</Button>
-    <Heading level={2}>Customized buttons</Heading>
+    <Heading level={2}>Standard Buttons</Heading>
+    <Button>Rounded </Button>
+    <Button color="primary">Primary</Button>
+    <Button color="secondary">Secondary</Button>
+    <Button color="alert">Alert</Button>
+    <Button color="warning">Warning</Button>
+    <Button color="info">Info</Button>
+    <Heading level={2}>Rounded buttons</Heading>
     <Button rounded>Rounded </Button>
     <Button color="primary" rounded>
       Primary
     </Button>
-    <Button color="secondary">Secondary</Button>
+    <Button color="secondary" rounded>
+      Secondary
+    </Button>
+    <Button color="alert" rounded>
+      Alert
+    </Button>
+    <Button color="warning" rounded>
+      Warning
+    </Button>
+    <Button color="info" rounded>
+      Info
+    </Button>
   </Layout>
 )
 
